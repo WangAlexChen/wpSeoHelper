ce9421045f0622572f23effb313a199cdfe2054e
wang <869476711@qq.com>
2024-01-23 14:34:50 +0800

    第一次提交

--- test.php
+++ test.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * WpSeoHelper - Custom Content Types and Fields
+ *
+ * @package   WpSeoHelper
+ * @author    WangGe
+ * @copyright 2023 WangGe Foundation, Inc
+ * @license   GPL v2 or later
+ *
+ * Plugin Name:       WpSeoHelper
+ * Plugin URI:        https://wanggetop.cn/
+ * Description:       Seo
+ * Version:           1.0.0
+ * Author:            WangGe Team
+ * Author URI:        https://wanggetop.cn/
+ * Text Domain:       Wordpress Seo
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 6.0
+ * Requires PHP:      7.3
+ * GitHub Plugin URI: https://github.com/WangAlexChen/wpSeoHelper
+ * Primary Branch:    main
+ */
+
+defined( 'ABSPATH' ) || exit;
+
  echo 'hello word!';
  
  
